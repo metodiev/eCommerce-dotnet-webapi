a4dfe605af44266dd1d6420adaa557e5cb3b968f
Anton Kostov <tonigreat@abv.bg>
2025-07-14 19:13:52 -0700

    init paymentservice project with service interface and half the endpoints mapped to controller

---
+++ PaymentService/Controllers/v1/PaymentController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PaymentService.Services.Interfaces;
+
+namespace PaymentService.Controllers.v1
+{
+    [Route("api/v1/payments")]
+    [ApiController]
+    [Authorize]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+        /// <summary>
+        /// Should initialize the PaymentService through dependency injection
+        /// </summary>
+        /// <param name="paymentService">Service handling Payment-related business logic</param>
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+        /// <summary>
+        /// Initiate a new payment transaction
+        /// </summary>
+        /// <param name="payment">New Payment to create</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> CreatePaymentTransaction(object payment)
+        {
+            var newPayment = await _paymentService.CreatePaymentTransactionAsync(payment);
+            return Ok(newPayment);
+        }
+        /// <summary>
+        /// Get payment status/details by payment ID
+        /// </summary>
+        /// <param name="paymentId">Id of payment to fetch</param>
+        /// <returns></returns>
+        [HttpGet("{paymentId}")]
+        public async Task<IActionResult> GetPaymentDetailsById(int paymentId)
+        {
+            var payment = await _paymentService.GetPaymentDetailsByIdAsync(paymentId);
+            return Ok(payment);
+        }
+        /// <summary>
+        /// Issue a refund for a specific payment
+        /// </summary>
+        /// <param name="paymentId">Id of payment to refund</param>
+        /// <returns></returns>
+        [HttpPost("{paymentId}/refund")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RefundPayment(int paymentId)
+        {
+            var refundStatus = await _paymentService.RefundPaymentAsync(paymentId);
+            return Ok(refundStatus);
+        }
+
+        /// <summary>
+        /// Get inventory snapshot for a warehouse
+        /// </summary>
+        /// <param name="warehouseId">Id of warehouse of the inventory snapshot to fetch</param>
+        /// <returns></returns>
+        [HttpGet("/user/{userId}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllPaymentsByUserId(int userId)
+        {
+            var paymentList = await _paymentService.GetAllPaymentsByUserIdAsync(userId);
+            return Ok(paymentList);
+        }
+    }
+}
---
+++ PaymentService/Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentService.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<object> CreatePaymentTransactionAsync(object payment);
+        Task<IList<object>> GetAllPaymentsByUserIdAsync(int userId);
+        Task<object> GetPaymentDetailsByIdAsync(int paymentId);
+        Task<object> RefundPaymentAsync(int paymentId);
+    }
+}
