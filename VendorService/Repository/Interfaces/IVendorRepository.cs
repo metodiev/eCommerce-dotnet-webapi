4caecd3e6fb8809cb87ee9bacdd34b3bba34cb91
Anton Kostov <tonigreat@abv.bg>
2025-07-16 19:00:52 -0700

    initialized vendor service skeleton

---
+++ VendorService/Repository/Interfaces/IVendorRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendorService.Repository.Interfaces
+{
+    public interface IVendorRepository
+    {
+        Task<IList<object>> GetProductsByVendorIdAsync(int vendorId);
+        Task<IList<object>> GetRegisteredVendorsAsync();
+        Task<object> GetVendorDetailsByIdAsync(int vendorId);
+        Task<object> GetVendorStatusByVendorIdAsync(int vendorId);
+        Task<IList<object>> MapVendorProductToInternalCatalogAsync(int vendorId, IList<object> vendorProducts);
+        Task<object> OnboardVendorAsync(object vendor);
+        Task<object> UpdateVendorAsync(int vendorId, object vendorInfo);
+        Task<IList<object>> UpdateVendorInventoryFeedAsync(int vendorId, IList<object> inventoryFeed);
+    }
+}
