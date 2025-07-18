7f2d7914d9fc6e8c16e8482a603a69f385c81f23
Anton Kostov <tonigreat@abv.bg>
2025-07-17 18:36:11 -0700

    added poromotioncontroller endpoints and service/repo ifaces

---
+++ PromotionService/Repository/Interfaces/IPromotionRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionService.Repository.Interfaces
+{
+    public interface IPromotionRepository
+    {
+    }
+}
