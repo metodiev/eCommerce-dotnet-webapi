e5d01c82f1ffcff57f11810bb53b8f4fa3016c91
Anton Kostov <tonigreat@abv.bg>
2025-07-23 18:47:56 -0700

    added configrepo implementation

---
+++ ConfigService/Repository/ConfigRepository.cs
@@ -0,0 +1,60 @@
+﻿using ConfigService.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigService.Repository
+{
+    public class ConfigRepository : IConfigRepository
+    {
+        public Task<object> CreateFeatureFlagAsync(object featureFlag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> CreateSystemSettingAsync(object setting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteFeatureFlagAsync(int flagId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteSystemSettingAsync(int settingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<object>> GetAllFeatureFlagsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<object>> GetAllRecentLogsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<object>> GetAllSystemSettingsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HealthCheckAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> UpdateFeatureFlagAsync(int flagId, object featureFlag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> UpdateSystemSettingAsync(int settingId, object setting)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
