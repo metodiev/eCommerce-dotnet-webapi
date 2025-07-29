ae628cec48081908197d368c46ac7a8a9740640c
Anton Kostov <139926184+almondlover@users.noreply.github.com>
2025-07-29 09:14:18 -0700

    Sprint 1: Feature/EN-7/.NET API Implementation - Payment Service (#5)
    Initialized Payment service project with a controller with mapped endpopints, connected to paymentservice class and  initialized structure with Controllers, Services, Repositories layers with interfaces and implementations for Service/Repository classes

---
+++ PaymentService/Repository/PaymentRepository.cs
@@ -0,0 +1,55 @@
+﻿using PaymentService.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentService.Repository
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        public Task<object> AddPaymentMethodForUserAsync(object paymentMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> CreatePaymentTransactionAsync(object payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteSavedPaymentMethodAsync(int methodId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<object>> GetAllPaymentsByUserIdAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetExternalPaymentProvidersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetPaymentDetailsByIdAsync(int paymentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<object>> GetPaymentMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> RefundPaymentAsync(int paymentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ValidatePaymentAsync(object validatedPayment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
