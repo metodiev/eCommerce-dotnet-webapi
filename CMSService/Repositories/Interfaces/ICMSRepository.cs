a56eee35083d6959cf23723016ff52982a1d497e
Anton Kostov <139926184+almondlover@users.noreply.github.com>
2025-09-02 18:48:10 +0300

    Sprint 2: Feature/EN-16/.NET API Implementation - CMS Service (#18)
    * added CMSservice project w/ controller w/ mapped endpointes as per documentation, CMS service&repo interfaces, implementations
    
    * changed ids to string
    
    ---------
    
    Co-authored-by: Kiril Syuleyman <93580277+kir40lmao@users.noreply.github.com>

---
+++ CMSService/Repositories/Interfaces/ICMSRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMSService.Repositories.Interfaces
+{
+    public interface ICMSRepository
+    {
+        Task<object> CreateBlogPostAsync(object blog);
+        Task<object> CreateCMSPageAsync(object page);
+        Task<object> CreateFAQAsync(object faq);
+        Task DeleteBlogPostAsync(string blogId);
+        Task DeleteCMSPageAsync(string pageId);
+        Task DeleteFAQAsync(string faqId);
+        Task<IList<object>> GetAllCMSPagesAsync();
+        Task<object> GetBlogPostByIdAsync(string blogId);
+        Task<IList<object>> GetBlogPostsAsync();
+        Task<object> GetCMSPageByIdAsync(string pageId);
+        Task<IList<object>> GetFAQListAsync();
+        Task HealthCheckAsync();
+        Task<object> UpdateBlogPostAsync(string blogId, object blog);
+        Task<object> UpdateCMSPageAsync(string pageId, object page);
+        Task<object> UpdateFAQAsync(string faqId, object faq);
+    }
+}
